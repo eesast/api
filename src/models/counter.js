f5e90c51463b4608553efc25e0b6656d4763cac8
Robert Ying <yingrui205@gmail.com>
2018-12-03 23:59:05 -0700

    Add auto increment counter

---
+++ src/models/counter.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+/**
+ * Used for auto increment id
+ */
+const counterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  count: { type: Number, default: 0 }
+});
+
+export default mongoose.model("Counter", counterSchema);
