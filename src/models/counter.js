3b0f652dd8bf5f5c2e4948b4f3918ed27ef17066
Robert Ying <yingrui205@gmail.com>
2018-12-04 23:33:49 -0700

    Declare collection name for counter

--- src/models/counter.js
+++ src/models/counter.js
@@ -3,9 +3,14 @@ import mongoose from "mongoose";
 /**
  * Used for auto increment id
  */
-const counterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  count: { type: Number, default: 0 }
-});
+const counterSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    count: { type: Number, default: 0 }
+  },
+  {
+    collection: "counters"
+  }
+);
 
 export default mongoose.model("Counter", counterSchema);
