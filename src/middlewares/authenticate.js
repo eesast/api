e99f34a3f3ee664fbfba6ebf1067c3a0529676c1
Robert Ying <yingrui205@gmail.com>
2018-12-04 22:54:42 -0700

    Add authenticate middleware

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+import secret from "../config/secret";
+
+const authenticate = requiredRoles => {
+  return (
+    authenticate[requiredRoles] ||
+    (authenticate[requiredRoles] = (req, res, next) => {
+      const authHeader = req.get("Authorization");
+      if (!authHeader)
+        return res.status(401).send("401 Unauthorized: Missing token");
+
+      const token = authHeader.substring(7);
+      jwt.verify(token, secret, (err, decoded) => {
+        if (err)
+          res.status(401).send("401 Unauthorized: Token expired or invalid");
+        req.auth = decoded;
+        delete decoded.reset;
+        delete decoded.iat;
+        delete decoded.exp;
+
+        User.findOne(decoded, (err, user) => {
+          if (err) return res.status(500).end();
+
+          if (!requiredRoles.includes(user.role)) {
+            return res.status(401).send("401 Unauthorized: Permission denied");
+          }
+
+          next();
+        });
+      });
+    })
+  );
+};
+
+export default authenticate;
