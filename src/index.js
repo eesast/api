74026cf325105a42c50a3f8d495bbf18db3fc056
Robert Ying <yingrui205@gmail.com>
2019-01-05 06:02:47 +0800

    Use host.docker.internal to connect to db

--- src/index.js
+++ src/index.js
@@ -13,8 +13,11 @@ const debug = Debug("sast-app-api");
 /**
  * Connect database.
  */
+const localhost =
+  process.env.LOCALHOST ||
+  (process.env.NODE_ENV === production ? "host.docker.internal" : "localhost");
 mongoose.connect(
-  "mongodb://localhost:27017/sast-app-api",
+  `mongodb://${localhost}:27017/sast-app-api`,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
