9f9dd769faa3497af322fe76893616be53619074
Robert Ying <yingrui205@gmail.com>
2019-01-08 23:40:29 +0800

    Expose Location header in cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ import serverConfig from "./config/server";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    exposedHeaders: "Location"
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
