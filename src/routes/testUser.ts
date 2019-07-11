f239e19b1f3357e5260e88757d8a7affcae149a6
zxdclyz <1515685918@qq.com>
2019-07-11 14:34:04 +0800

    add router testUser for homework

---
+++ src/routes/testUser.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+
+const router = express.Router();
+
+router.post('/', (req, res)=>{
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password){
+        return res
+            .status(422)
+            .send("information incomplete");
+    }
+    else {
+        return res
+            .status(200)
+            .send("login successfully");
+    }
+})
+
+export default router;
