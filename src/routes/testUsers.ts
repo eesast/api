5a49deaa1a0f59697f2490863c361ad5e39977f1
Xiaoyang Ma <boltma@gmail.com>
2019-07-12 08:27:13 +0800

    Fix bug of checking null of undefined strings

--- src/routes/testUsers.ts
+++ src/routes/testUsers.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  if (username !== "" && password !== "") {
+  if (username && password) {
     return res.status(201).send("complete");
   } else {
     return res.status(422).send("incomplete");
