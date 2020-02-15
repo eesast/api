2ddd799b74b364252ea4ca92b237fecf288a8c90
Campbell <duskmoon314@users.noreply.github.com>
2020-02-15 21:55:49 +0800

    :sparkles: Add authorId & createdBy to getArticle (#332)
    * :sparkles: Add authorId & createdBy to getArticle
    
    * :sparkles: Add authorId & createdBy to getArticle
    
    * :sparkles::goal_net: Add count to get number of articles & Prevent some errors
    
    * :sparkles: get under review articles

--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -2,13 +2,15 @@ import express from "express";
 import authenticate from "../middlewares/authenticate";
 import Article from "../models/article";
 import pick from "lodash.pick";
+import checkToken from "../middlewares/checkToken";
 
 const router = express.Router();
 
 /**
  * GET articles with queries
  * @param {string} title - Title will be partial matched
- * @param {string} author
+ * @param {number} authorId
+ * @param {number} createdBy
  * @param {string} alias
  * @param {string} tag
  * @param {number} likedBy - liker's Id
@@ -16,14 +18,25 @@ const router = express.Router();
  * @param {number} end
  * @param {boolean} noContent
  * @param {boolean} invisible
+ * @param {boolean} count
  * @returns {Object[]} certain articles
  */
-router.get("/", async (req, res, next) => {
+router.get("/", checkToken, async (req, res, next) => {
   const query = {
-    ...pick(req.query, ["author", "alias", "tag", "likedBy"]),
+    ...pick(req.query, [
+      "author", // TODO: 实现这个字段的正常查询
+      "authorId",
+      "createdBy",
+      // "tag",
+      "alias",
+      "likedBy"
+    ]),
     ...(req.query.title && {
       title: { $regex: req.query.title, $options: "i" }
     }),
+    // ...(req.query.tag && {
+    //   tags: { $in: req.query.tag }
+    // }),
     visible: !req.query.invisible
   };
 
@@ -33,6 +46,63 @@ router.get("/", async (req, res, next) => {
     "-_id -__v" + (req.query.noContent === true ? " -content" : "");
 
   try {
+    if (
+      !query.visible &&
+      ((req.auth.role !== "root" && req.auth.role !== "editor") ||
+        ((query.authorId || query.createdBy) &&
+          req.auth.id !== (query.authorId || query.createdBy)))
+    ) {
+      return res
+        .status(403)
+        .send(
+          "403 Forbidden: You are not allow to view others' private article"
+        );
+    }
+
+    if (req.query.count) {
+      if (query.authorId) {
+        const num = await Article.count({
+          authorId: query.authorId,
+          visible: query.visible
+        });
+        return res.json({ num: num });
+      }
+      if (query.createdBy) {
+        const num = await Article.count({
+          createdBy: query.createdBy,
+          visible: query.visible
+        });
+        return res.json({ num: num });
+      }
+      if (
+        !query.visible &&
+        (req.auth.role === "root" || req.auth.role === "editor")
+      ) {
+        const num = await Article.count({
+          visible: query.visible,
+          tags: { $in: ["underReview"] }
+        });
+        return res.json({ num: num });
+      } else return res.status(403).send("403 Forbidden: Forbidden behavior");
+    }
+
+    if (
+      !query.visible &&
+      (req.auth.role === "root" || req.auth.role === "editor") &&
+      req.query.tag === "underReview"
+    ) {
+      const articles = await Article.find(
+        { ...query, tags: { $in: [req.query.tag] } },
+        select,
+        {
+          skip: begin,
+          limit: end - begin + 1,
+          sort: "-createdAt"
+        }
+      );
+      res.json(articles);
+    }
+
     const articles = await Article.find(query, select, {
       skip: begin,
       limit: end - begin + 1,
@@ -121,6 +191,15 @@ router.get("/:id/unlike", authenticate([]), async (req, res, next) => {
  */
 router.post("/", authenticate(["root", "writer"]), async (req, res, next) => {
   try {
+    if (await Article.findOne({ alias: req.body.alias })) {
+      res.setHeader("Location", "/articles");
+      return res.status(409).send("409 Conflict: Alias already exists");
+    }
+
+    if (req.body.content === "") {
+      return res.status(422).send("422 UnProcessable Entity: Missing contents");
+    }
+
     const article = await new Article({
       ...req.body,
       createdBy: req.auth.id,
@@ -153,6 +232,34 @@ router.put(
       if (req.auth.selfCheckRequired) {
         if (article.authorId !== req.auth.id) {
           return res.status(401).send("401 Unauthorized: Permission denied");
+        }
+      }
+
+      if (req.body.visible === true) {
+        const article = await Article.findOne({ id: req.params.id });
+        if (article?.tags.includes("underReview")) {
+          if (req.auth.role !== "root" && req.auth.role !== "editor")
+            return res
+              .status(403)
+              .send("403 Forbidden: Article is under review");
+          else {
+            const update = {
+              ...req.body,
+              tags: req.body.tags
+                ? req.body.tags.splice(req.body.tags.indexOf("underReview"), 1)
+                : article.tags.splice(article.tags.indexOf("underReview"), 1),
+              updatedAt: new Date(),
+              updatedBy: req.auth.id
+            };
+
+            const newArticle = await Article.findOneAndUpdate(
+              { id: req.params.id },
+              update
+            );
+
+            res.setHeader("Location", "/v1/articles/" + newArticle!.id);
+            return res.status(204).end();
+          }
         }
       }
 
