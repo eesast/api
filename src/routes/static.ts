760a61d8b1a7530e6af9018b9e5d9006f434f30a
BryantSuen <65460093+BryantSuen@users.noreply.github.com>
2022-04-21 01:41:38 +0800

    Oss permission (#1017)
    * remove docker_cron and routes for THUAI4
    
    * remove docker_cron and routes for THUAI4
    
    * ci: :arrow_down: downgrade node to v16
    
    * feat: :sparkles: add shared oss route

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -27,6 +27,28 @@ router.get("/sts", authenticate(["counselor", "root"]), async (req, res) => {
     });
 });
 
+router.get("/shared", authenticate(["counselor", "root", "student"]), async (req, res) => {
+  const client = new STS({
+    accessKeyId: process.env.OSS_KEY_ID!,
+    accessKeySecret: process.env.OSS_KEY_SECRET!,
+  });
+
+  client
+    .assumeRole(process.env.OSS_ROLE_ARN, policy, 3600)
+    .then((result: any) => {
+      res.status(200).json({
+        AccessKeyId: result.credentials.AccessKeyId,
+        AccessKeySecret: result.credentials.AccessKeySecret,
+        SecurityToken: result.credentials.SecurityToken,
+        Expiration: result.credentials.Expiration,
+      });
+    })
+    .catch((err: Error) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
 router.get("/*", async (req, res) => {
   const path = req.url;
 
