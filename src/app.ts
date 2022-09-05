34a1756d0d393de2da0529df0d3fd581fff5ce5c
L0510410 <1847254187@qq.com>
2022-09-05 21:51:54 +0800

    update route for docs

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import staticRouter from "./routes/static";
 import userRouter from "./routes/users";
 import emailRouter from "./routes/emails";
 import weeklyRouter from "./routes/weekly";
+import docsRouter from "./routes/docs";
 //import codeRouter from "./routes/code";
 //import roomRouter from "./routes/room";
 //import contestRouter from "./routes/contest";
@@ -13,7 +14,7 @@ const app = express();
 
 const whitelist =
   process.env.NODE_ENV === "production"
-    ? ["https://eesast.com", "http://localhost:3000"]
+    ? ["https://eesast.com", "https://docs.eesast.com", "http://localhost:3000"]
     : ["http://localhost:3000"];
 
 app.use(
@@ -36,6 +37,7 @@ app.use("/static", staticRouter);
 app.use("/users", userRouter);
 app.use("/emails", emailRouter);
 app.use("/weekly", weeklyRouter);
+app.use("/docs", docsRouter);
 //app.use("/code", codeRouter);
 //app.use("/room", roomRouter);
 //app.use("/contest", contestRouter);
---
+++ src/routes/docs.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const router = express.Router();
+
+router.get("/get_video_link", async (req, res) => {
+    try {
+        if (!req.query.url) return res.status(400).send("400 Bad Request: no url provided!");
+        console.log(req.query.url);
+        const url: any = req.query.url;
+        const response = await fetch(
+            url,
+            { method: "GET"}
+        );
+        if (response.ok) {
+            const text: string = await response.text();
+            const match = text.match(/rawPath:.*/);
+            if (!match) throw(Error("cannot match pattern!"));
+            let link: string = match[0].slice(10, -2);
+            link = link.replace(/\\u002D/g, "-");
+            return res.status(200).send(link);
+        }
+        else return res.status(500).send("500 Internal Server Error: fetch failed!");
+    } catch (err) {
+        return res.status(500).send("500 Internal Server Error: " + err);
+    }
+})
+
+export default router;
