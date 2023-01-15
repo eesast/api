d1147ee675932d884664702862872afe07a09afa
L0510410 <1847254187@qq.com>
2023-01-15 11:42:50 +0800

    fix the file uploading function

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,16 +1,32 @@
 import express from "express";
-import upload from "../models/file";
+import fs from "fs";
+import path from "path";
 const router = express.Router();
-router.post('/upload', async(req, res)=> {
-    await upload(req, res);
-    const file = req.file;
-    console.log(file);
-    if (file) {
-      return res.status(200).json({ success: true });
-    } else {
-      return res.status(500).json({ success: false });
-    }
 
-  });
+const mkdirsSync = (dirname: string) => {
+  if (fs.existsSync(dirname)) {
+    return true;
+  } else {
+    if (mkdirsSync(path.dirname(dirname))) {
+      fs.mkdirSync(dirname);
+      return true;
+    }
+  }
+}
+
+router.post('/upload', async (req, res) => {
+  try{
+    const file: string = req.body.file;
+    const dest: string = req.body.dest;
+    mkdirsSync(path.dirname(dest));
+    fs.writeFile(dest, file, 'utf8', function(err) {
+      if(err) throw err;
+      else return res.status(200).send("ok!");
+    });
+  }
+  catch (err) {
+    return res.status(500).send(err);
+  }
+});
 
 export default router;
