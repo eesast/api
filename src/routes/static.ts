14156b92da8e6106d64e71d8ab52e2ee367a7d52
Tian Guoyun <88619302+BlitherBoom812@users.noreply.github.com>
2023-04-03 19:56:14 +0800

    fix the bug of get "/static/player" and add verification (#1222)
    * add route get /static/player
    
    * mergify.yml
    
    * verify whether users are in their team
    fix the bug of get "/static/player"

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -1,6 +1,9 @@
 import express from "express";
-import authenticate from "../middlewares/authenticate";
+import authenticate, { JwtPayload } from "../middlewares/authenticate";
 import getSTS from "../helpers/sts";
+import { client } from "..";
+import { gql } from "graphql-request";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -26,26 +29,91 @@ router.get("/", authenticate(["counselor", "root"]), async (req, res) => {
 });
 
 
-// 用户具有的权限：  "name/cos:PutObject",   "name/cos:GetObject", "   "name/cos:DeleteObject", 可访问的目录为/THUAI6/{team_id}/\*
-/**
- * POST compile code of team_id
- * @param token (user_id)
- * @param {uuid} req.body.team_id
- */
-router.get("/player", authenticate(), async (req, res) => {
-  try {
-    const action = [
-      "name/cos:PutObject",
-      "name/cos:GetObject",
-      "name/cos:DeleteObject",
-    ];
-    const sts = await getSTS(action, `/THUAI6/${req.body.team_id}/*`);
-    return res.status(200).send(sts);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send(err);
-  }
-});
 
+// 用户具有的权限:"name/cos:PutObject","name/cos:InitiateMultipartUpload","name/cos:ListMultipartUploads","name/cos:ListParts","name/cos:UploadPart","name/cos:CompleteMultipartUpload","name/cos:AbortMultipartUpload","name/cos:GetObject","name/cos:DeleteObject",
+// 可访问的目录为/THUAI6/{team_id}/\*
+/**
+ * GET compile code of team_id
+ * @param token (user_id)
+ * @param {uuid} req.query.team_id
+ */
+router.get("/player", async (req, res) => {
+  const team_id = req.query.team_id;
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    return res.status(401).send("401 Unauthorized: Missing token");
+  }
+  const token = authHeader.substring(7);
+  return jwt.verify(token, process.env.SECRET!, async (err, decoded) => {
+    if (err || !decoded) {
+      return res
+        .status(401)
+        .send("401 Unauthorized: Token expired or invalid");
+    }
+    const payload = decoded as JwtPayload;
+    const user_id = payload._id;
+    const query_if_manager = await client.request(
+      gql`
+        query query_is_manager($contest_id: uuid, $user_id: String) {
+          contest_manager(where: {_and: {contest_id: {_eq: $contest_id}, user_id: {_eq: $user_id}}}) {
+            user_id
+          }
+        }
+      `,
+      { contest_id: process.env.GAME_ID, user_id: user_id }
+    );
+    const is_manager = query_if_manager.contest_manager.length != 0;
+    if (!is_manager) {
+      try {
+        const query_in_team = await client.request(
+          gql`
+            query query_if_in_team($team_id: uuid, $user_id: String, $contest_id: uuid) {
+              contest_team(
+                where: {
+                  _and: [
+                    { contest_id: { _eq: $contest_id } }
+                    { team_id: { _eq: $team_id } }
+                    {
+                      _or: [
+                        { team_leader: { _eq: $user_id } }
+                        { contest_team_members: { user_id: { _eq: $user_id } } }
+                      ]
+                    }
+                  ]
+                }
+              ) {
+                team_id
+              }
+            }
+          `,
+          { contest_id: process.env.GAME_ID, team_id: team_id, user_id: user_id }
+        );
+        const is_in_team = query_in_team.contest_team.length != 0;
+        if (!is_in_team) return res.status(401).send("当前用户不在队伍中");
+      } catch (err) {
+        return res.status(400).send(err);
+      }
+    }
+    try {
+      const action = [
+        "name/cos:PutObject",
+        "name/cos:InitiateMultipartUpload",
+        "name/cos:ListMultipartUploads",
+        "name/cos:ListParts",
+        "name/cos:UploadPart",
+        "name/cos:CompleteMultipartUpload",
+        "name/cos:AbortMultipartUpload",
+        "name/cos:GetObject",
+        "name/cos:DeleteObject",
+      ];
+      // console.log(req.query.team_id)
+      const sts = await getSTS(action, `THUAI6/${req.query.team_id}/*`);
+      return res.status(200).send(sts);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+  });
+});
 
 export default router;
