b47fb10514de4a59c5af39ab57cb5d65331e94ff
FranGuam <frankguansy@icloud.com>
2024-02-29 23:43:28 +0800

    fix(notification): renew subscriptions & resolve for timeout

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { setVapidDetails, sendNotification } from 'web-push';
+import { sendNotification } from 'web-push';
 import authenticate from "../middlewares/authenticate";
 import crypto from 'crypto';
 
@@ -25,12 +25,6 @@ const router = express.Router();
 
 const subscriptions: any = {};
 
-setVapidDetails(
-  "https://eesast.com",
-  VAPID_PUBLIC_KEY,
-  VAPID_PRIVATE_KEY
-);
-
 const hash = (input: string) => {
   const md5sum = crypto.createHash("md5");
   md5sum.update(Buffer.from(input));
@@ -43,18 +37,38 @@ router.post("/subscribe", authenticate(), (req, res) => {
   return res.status(200).json({ index: index });
 });
 
+router.post("/check", authenticate(), (req, res) => {
+  const index = hash(req.body.endpoint);
+  return res.status(200).json({ subscribed: !!subscriptions[index] });
+});
+
 router.post("/unsubscribe", authenticate(), (req, res) => {
   const index = req.body.index;
+  if (!subscriptions[index]) return res.status(200).json({ message: "Not found" });
   delete subscriptions[index];
   return res.status(200).json({ message: "Unsubscribed" });
 });
 
-router.post("/broadcast", (req, res) => {
+router.post("/broadcast", async (req, res) => {
+  const notification = JSON.stringify(req.body as Notification);
+  const options = {
+    timeout: 1000,
+    vapidDetails: {
+      subject: "https://eesast.com",
+      publicKey: VAPID_PUBLIC_KEY,
+      privateKey: VAPID_PRIVATE_KEY
+    },
+  };
   for (const index in subscriptions) {
     console.log(index);
     const subscription = subscriptions[index];
     try {
-      sendNotification(subscription, JSON.stringify(req.body as Notification));
+      const response = await sendNotification(subscription, notification, options);
+      if (response.statusCode === 201) {
+        console.log("Notification sent");
+      } else {
+        console.error("Notification failed");
+      }
     } catch (e) {
       console.error(e);
     }
