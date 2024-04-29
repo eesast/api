b029514c36fc8d8b930be785a6637adaf9b4de50
Guoyun Tian <guoyun812@qq.com>
2024-04-29 19:13:17 +0800

    fix(bugs): utils data yarn lint check

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -140,6 +140,9 @@ export async function uploadObject(localFilePath: string, bucketKey: string, cos
         console.log(err);
         reject('Failed to upload object to COS');
       } else {
+        if (data) {
+          console.debug('Upload Success');
+        }
         // console.debug('Upload Success', data);
         resolve(true);
       }
