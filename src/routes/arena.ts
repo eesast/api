d046f292184fb4a6634a4ee5eaeb03337d80119d
Guoyun Tian <88619302+BlitherBoom812@users.noreply.github.com>
2024-05-08 03:23:24 +0800

    fix(bugs): COS filename (#1337)
    * fix(bugs): COS filename
    
    * resume comment
    
    ---------
    
    Co-authored-by: FranGuam <53105842+FranGuam@users.noreply.github.com>

--- src/routes/arena.ts
+++ src/routes/arena.ts
@@ -456,8 +456,8 @@ router.post("/finish", async (req, res) => {
           const config = await utils.getConfig();
           const file_name = await fs.readdir(`${base_directory}/${contest_name}/arena/${room_id}/output`);
           const upload_file_promises = file_name.map(filename => {
-            const suffix = filename.split(".")[1];
-            const key = `${contest_name}/arena/${room_id}/${room_id}.${suffix}`;
+            console.log("filename: " + filename)
+            const key = `${contest_name}/arena/${room_id}/${filename}`;
             const localFilePath = `${base_directory}/${contest_name}/arena/${room_id}/output/${filename}`;
             return utils.uploadObject(localFilePath, key, cos, config)
               .then(() => {
