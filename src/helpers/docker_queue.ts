4091ea3956fcccb0c1fc286a6f0e80fa9f8531ab
chenziyi <c-zy22@mails.tsinghua.edu.cn>
2024-05-13 20:58:02 +0800

    Fix rebase bug

--- src/helpers/docker_queue.ts
+++ src/helpers/docker_queue.ts
@@ -170,10 +170,6 @@ const docker_cron = async () => {
             return;
           }
 
-          // port 始终需要使用
-          await hasura.update_room_port(queue_front.room_id, port);
-          await hasura.update_room_created_at(queue_front.room_id, new Date().toISOString());
-
           console.log("room status updated");
 
           if (queue_front.envoy === 1) {
