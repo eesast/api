55168ce6ae46491a71d1540af489ea54dede8a4b
zzdhyb <139191187+zzdhybthu@users.noreply.github.com>
2024-09-11 15:12:37 +0800

    feat: update honor application (#1431)
    * feat: update honor application
    
    * chore

--- src/routes/application.ts
+++ src/routes/application.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { gql } from "graphql-request";
 import { client } from "..";
 import * as MentHasFunc from "../hasura/mentor";
+import * as HnrHasFunc from "../hasura/honor";
 
 const router = express.Router();
 
@@ -38,11 +39,144 @@ router.get("/info/honor", async (req, res) => {
             `
         )
         const types: string[] = q_honor_type.honor_type.map((item: { type_name: string }) => item.type_name);
-        return res.status(200).send({types: types, time: q_honor_time.honor_time_by_pk});
+        return res.status(200).send({ types: types, time: q_honor_time.honor_time_by_pk });
     } catch (err) {
         return res.status(500).send(err);
     }
 })
+
+router.post("/honor/insert_one", async (req, res) => {
+    try {
+        const student_uuid: string = req.body.student_uuid;
+        const honor: string = req.body.honor;
+        const statement: string = req.body.statement ?? "";
+        const attachment_url: string = req.body.attachment_url ?? undefined;
+
+        if (!student_uuid || !honor) {
+            return res.status(450).send("Error: Missing student_uuid or honor");
+        }
+
+        const role = await HnrHasFunc.query_user_role(student_uuid);
+        if (role !== "student" && role !== "counselor") {
+            return res.status(451).send("Error: Invalid role");
+        }
+
+        const year: number = new Date().getFullYear();
+        const insert_id = await HnrHasFunc.insert_honor_application(student_uuid, honor, statement, attachment_url, year);
+        if (!insert_id) {
+            return res.status(452).send("Error: Insert honor application failed");
+        }
+        return res.status(200).send(insert_id);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send(err);
+    }
+})
+
+router.post("/honor/update_one", async (req, res) => {
+    try {
+        const id: string = req.body.id;
+        const honor: string = req.body.honor;
+        const statement: string = req.body.statement ?? "";
+        const attachment_url: string = req.body.attachment_url ?? undefined;
+        const student_uuid: string = req.body.student_uuid;
+
+        if (!id || !honor || !student_uuid) {
+            return res.status(450).send("Error: Missing id or honor or student_uuid");
+        }
+
+        const application = await HnrHasFunc.query_honor_application(id);
+        const role = await HnrHasFunc.query_user_role(student_uuid);
+        if (!application) {
+            return res.status(451).send("Error: No honor application found");
+        } else if (application.student_uuid !== student_uuid && role !== "counselor") {
+            return res.status(452).send("Error: Invalid student_uuid");
+        }
+
+        const year: number = new Date().getFullYear();
+        if (application.year !== year) {
+            return res.status(453).send("Error: Invalid year");
+        }
+
+        if (!attachment_url) {
+            const response = await HnrHasFunc.update_honor_application(id, honor, statement);
+            if (!response) {
+                return res.status(454).send("Error: Update honor application failed");
+            }
+        } else {
+            const response = await HnrHasFunc.update_honor_application_with_attachment(id, honor, statement, attachment_url);
+            if (!response) {
+                return res.status(454).send("Error: Update honor application failed");
+            }
+        }
+        return res.status(200).send(id);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send(err);
+    }
+})
+
+router.post("/honor/delete_one", async (req, res) => {
+    try {
+        const id: string = req.body.id;
+        const student_uuid: string = req.body.student_uuid;
+
+        if (!id || !student_uuid) {
+            return res.status(450).send("Error: Missing id or student_uuid");
+        }
+
+        const application = await HnrHasFunc.query_honor_application(id);
+        const role = await HnrHasFunc.query_user_role(student_uuid);
+        if (!application) {
+            return res.status(451).send("Error: No honor application found");
+        } else if (application.student_uuid !== student_uuid && role !== "counselor") {
+            return res.status(452).send("Error: Invalid student_uuid");
+        }
+
+        const year: number = new Date().getFullYear();
+        if (application.year !== year) {
+            return res.status(453).send("Error: Invalid year");
+        }
+
+        const response = await HnrHasFunc.delete_honor_application(id);
+        if (!response) {
+            return res.status(454).send("Error: Delete honor application failed");
+        }
+        return res.status(200).send(id);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+})
+
+router.post("/honor/update_status_one", async (req, res) => {
+    try {
+        const id: string = req.body.id;
+        const status: string = req.body.status;
+        const counselor_uuid: string = req.body.counselor_uuid;
+
+        if (!id || !status || !counselor_uuid) {
+            return res.status(450).send("Error: Missing id or status or counselor_uuid");
+        }
+
+        const application = await HnrHasFunc.query_honor_application(id);
+        const role = await HnrHasFunc.query_user_role(counselor_uuid);
+        if (!application) {
+            return res.status(451).send("Error: No honor application found");
+        } else if (role !== "counselor") {
+            return res.status(452).send("Error: Invalid counselor_uuid");
+        }
+
+        const response = await HnrHasFunc.update_honor_application_status(id, status);
+        if (!response) {
+            return res.status(453).send("Error: Update honor application status failed");
+        }
+        return res.status(200).send(id);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+})
+
+
 
 router.get("/info/mentor/:year", async (req, res) => {
     try {
@@ -64,14 +198,13 @@ router.get("/info/mentor/:year", async (req, res) => {
                     matched_applicants: applicationsApprovedCount
                 }
             }
-        ));
+            ));
         return res.status(200).send(info);
     } catch (err) {
         return res.status(500).send(err);
     }
 })
 
-// const insert_mentor_application = async (mentor_uuid: string, student_uuid: string, year: number, statement: string) => {
 
 router.post("/mentor/insert_one", async (req, res) => {
     const mentor_uuid: string = req.body.mentor_uuid;
