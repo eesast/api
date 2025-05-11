a5d4b2257e703d4b7ac8d01ac3991a31801f9c90
konpoku <bsy998244353@gmail.com>
2025-05-11 22:53:11 +0800

    feat: added log for weekly update

--- src/routes/weekly.ts
+++ src/routes/weekly.ts
@@ -5,27 +5,36 @@ import axios from "axios";
 import * as utils from "../helpers/utils";
 import * as fs from "fs/promises";
 import * as uuid from "../helpers/uuid";
-import { get_newest_weekly, get_newest_weekly_id, add_weekly_list, WeeklyPost, check_weekly_exist } from "../hasura/share"
+import {
+  get_newest_weekly,
+  get_newest_weekly_id,
+  add_weekly_list,
+  WeeklyPost,
+  check_weekly_exist,
+} from "../hasura/share";
 import authenticate from "../middlewares/authenticate";
 import { Agent } from "https";
 const router = express.Router();
 const weixinSpider = async (headers: any, params: any, filename: string) => {
   const url = "https://mp.weixin.qq.com/cgi-bin/appmsg";
-  let fcontrol :boolean = false;
+  let fcontrol: boolean = false;
   const base_directory = await utils.get_base_directory();
   try {
-    console.log("Spider Start")
+    console.log("Spider Start");
     const new_weekly_list: any[] = [];
     let i: number = 0;
-    outerloop:
-    while (!fcontrol) {
+    const newest_weekly_date = await get_newest_weekly();
+    let newest_weekly_id = await get_newest_weekly_id();
+    outerloop: while (!fcontrol) {
       params["begin"] = (i * 5).toString();
       i++;
-      await new Promise(resolve => setTimeout(resolve, Math.random() * 9000 + 1000)); // 等待 1 到 10 秒之间的随机时间
+      await new Promise((resolve) =>
+        setTimeout(resolve, Math.random() * 9000 + 1000),
+      ); // 等待 1 到 10 秒之间的随机时间
       const response = await axios.get(url, {
         headers,
         params,
-        httpsAgent: new Agent({ rejectUnauthorized: false })
+        httpsAgent: new Agent({ rejectUnauthorized: false }),
       });
       const data = response.data;
       if (data.base_resp.ret === 200013) {
@@ -34,15 +43,23 @@ const weixinSpider = async (headers: any, params: any, filename: string) => {
         break;
       }
       if (!data.app_msg_list || data.app_msg_list.length === 0) {
-        console.log('All article parsed');
+        console.log("All article parsed");
         break;
       }
-      const newest_weekly_date = await get_newest_weekly();
-      const newest_weekly_id = await get_newest_weekly_id();
+      console.log(
+        "Fetched " +
+          data.app_msg_list.length +
+          " articles after " +
+          new Date(data.app_msg_list[0].create_time * 1000).toLocaleString(),
+      );
       for (const item of data.app_msg_list) {
-        if (new Date(item.create_time * 1000) < newest_weekly_date) break outerloop;
+        if (new Date(item.create_time * 1000) <= newest_weekly_date)
+          break outerloop;
         if (item.title.includes("SAST Weekly")) {
-          const exist : boolean = await check_weekly_exist(new Date(item.create_time * 1000));
+          const exist: boolean = await check_weekly_exist(
+            new Date(item.create_time * 1000),
+          );
+
           if (exist) {
             continue;
           }
@@ -50,71 +67,80 @@ const weixinSpider = async (headers: any, params: any, filename: string) => {
             title: item.title,
             url: item.link,
             date: new Date(item.create_time * 1000),
-            id: newest_weekly_id + new_weekly_list.length + 1
-          }
+            id: newest_weekly_id + 1,
+          };
           new_weekly_list.push(new_item);
+          if (new_weekly_list.length > 0) {
+            await add_weekly_list(new_weekly_list);
+            new_weekly_list.length = 0;
+            newest_weekly_id++;
+          }
+          console.log("Fetched: " + item.title);
         }
       }
     }
-    new_weekly_list.sort((a, b) => {
-      return a.date.getTime() - b.date.getTime();
-    });
-    if (new_weekly_list.length > 0) {
-      await add_weekly_list(new_weekly_list);
-    }
-    //using uuid as the files name
-    if (!await utils.checkPathExists(`${base_directory}/weixinSpiderStatus`)) {
+    if (
+      !(await utils.checkPathExists(`${base_directory}/weixinSpiderStatus`))
+    ) {
       await fs.mkdir(`${base_directory}/weixinSpiderStatus`);
     }
     await fs.writeFile(`${base_directory}/weixinSpiderStatus/${filename}`, "");
     console.log("Spider finished");
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    await fs.writeFile(
+      `${base_directory}/weixinSpiderStatus/${filename}-failed`,
+      "",
+    );
   }
-  catch (error) {
-    console.error('Error fetching articles:', error);
-    await fs.writeFile(`${base_directory}/weixinSpiderStatus/${filename}-failed`, "");
-  }
-}
+};
 
 router.post("/check", authenticate(["counselor"]), async (req, res) => {
   try {
     const filename: string = req.body.filename;
     const base_directory = await utils.get_base_directory();
-    if (await utils.checkPathExists(`${base_directory}/weixinSpiderStatus/${filename}`)) {
-      return res.status(200).json({ finished: true , failed: false});
-    } else if (await utils.checkPathExists(`${base_directory}/weixinSpiderStatus/${filename}-failed`)) {
-      return res.status(200).json({ finished: false , failed: true});
+    if (
+      await utils.checkPathExists(
+        `${base_directory}/weixinSpiderStatus/${filename}`,
+      )
+    ) {
+      return res.status(200).json({ finished: true, failed: false });
+    } else if (
+      await utils.checkPathExists(
+        `${base_directory}/weixinSpiderStatus/${filename}-failed`,
+      )
+    ) {
+      return res.status(200).json({ finished: false, failed: true });
     } else {
-      return res.status(200).json({ finished: false , failed: false});
+      return res.status(200).json({ finished: false, failed: false });
     }
-  }
-  catch (err) {
+  } catch (err) {
     return res.status(500).send("500 Internal Server Error: " + err);
   }
-})
+});
 
 router.post("/renew", authenticate(["counselor"]), async (req, res) => {
-
   try {
     // 设置 headers
     const headers = {
-      "Cookie": req.body.cookie,
-      "User-Agent": req.body.useragent
+      Cookie: req.body.cookie,
+      "User-Agent": req.body.useragent,
     };
     const params = {
-      "token": req.body.token,
-      "lang": "zh_CN",
-      "f": "json",
-      "ajax": "1",
-      "action": "list_ex",
-      "begin": "1",
-      "count": "5",
-      "query": "",
-      "fakeid": "MzA5MjA5NjIxNg%3D%3D",
-      "type": "9",
-    }
+      token: req.body.token,
+      lang: "zh_CN",
+      f: "json",
+      ajax: "1",
+      action: "list_ex",
+      begin: "1",
+      count: "5",
+      query: "",
+      fakeid: "MzA5MjA5NjIxNg%3D%3D",
+      type: "9",
+    };
     const filename = uuid.uuid();
     weixinSpider(headers, params, filename);
-    return res.status(200).json({ "filename": filename });
+    return res.status(200).json({ filename: filename });
   } catch (err) {
     return res.status(500).send("500 Internal Server Error: " + err);
   }
@@ -193,172 +219,171 @@ router.post("/renew", authenticate(["counselor"]), async (req, res) => {
   //} catch (err) {
   //    return res.status(500).send("500 Internal Server Error: " + err);
   //}
-})
+});
 router.get("/cover", async (req, res) => {
   try {
-    if (!req.query.url) return res.status(400).send("400 Bad Request: no url provided!");
+    if (!req.query.url)
+      return res.status(400).send("400 Bad Request: no url provided!");
     const url: any = req.query.url;
-    const useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
-    const headers : HeadersInit = [
+    const useragent =
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
+    const headers: HeadersInit = [
       ["User-Agent", useragent],
-      ["Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"],
+      [
+        "Accept",
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      ],
       ["Accept-Language", "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4"],
       ["Accept-Encoding", "gzip, deflate, br"],
       ["Connection", "keep-alive"],
       ["Upgrade-Insecure-Requests", "1"],
-      ["Cache-Control", "max-age=0"]
-    ]
-    const response = await fetch(
-      url,
-      {
-        method: "GET",
-        headers: headers,
-      },
-    );
-    console.log(response);
+      ["Cache-Control", "max-age=0"],
+    ];
+    const response = await fetch(url, {
+      method: "GET",
+      headers: headers,
+    });
     if (response.ok) {
       const text: string = await response.text();
       const match = text.match(/var msg_cdn_url = "(.*?)";/);
-      if (match && match[1])
-        res.status(200).send(match[1]);
-      else throw (Error("capture failed!"));
-    }
-    else return res.status(500).send("500 Internal Server Error: fetch failed!");
+      if (match && match[1]) res.status(200).send(match[1]);
+      else throw Error("capture failed!");
+    } else
+      return res.status(500).send("500 Internal Server Error: fetch failed!");
   } catch (err) {
     return res.status(500).send("500 Internal Server Error: " + err);
   }
-})
+});
 
 const getTitle = async (url: string) => {
   try {
-    const response = await fetch(
-      url,
-      { method: "GET" }
-    );
+    const response = await fetch(url, { method: "GET" });
     if (response.ok) {
       const text: string = await response.text();
       const match = text.match(/meta property="og:title" content=".*"/);
-      if (match == null) throw (Error("capture failed!"));
+      if (match == null) throw Error("capture failed!");
       const title = match[0].slice(34, -1);
       return title;
-    }
-    else throw (Error("fetch failed!"));
+    } else throw Error("fetch failed!");
   } catch (err: any) {
     return err;
   }
-}
+};
 
-router.post("/insert",authenticate(["counselor"]), async (req, res) => {
+router.post("/insert", authenticate(["counselor"]), async (req, res) => {
   try {
-    if (!req.body.id || !req.body.url) return res.status(400).send("400 Bad Request: not enough params!");
+    if (!req.body.id || !req.body.url)
+      return res.status(400).send("400 Bad Request: not enough params!");
     const title: string = await getTitle(req.body.url);
     const QueryGreaterIds: any = await client.request(
       gql`
-              query QueryGreaterIds($_id: Int) {
-                weekly(where: {id: {_gt: $_id}}) {
-                  id
-                }
-              }
-            `,
-      { _id: req.body.id }
+        query QueryGreaterIds($_id: Int) {
+          weekly(where: { id: { _gt: $_id } }) {
+            id
+          }
+        }
+      `,
+      { _id: req.body.id },
     );
     const sorted_ids = [...QueryGreaterIds.weekly];
     sorted_ids.sort((a: any, b: any) => {
       return a.id - b.id;
-    })
+    });
     for (let i = sorted_ids.length - 1; i >= 0; i--) {
       await client.request(
         gql`
-                  mutation IncreaseIds($_id: Int) {
-                    update_weekly(where: {id: {_eq: $_id}}, _inc: {id: 1}) {
-                      affected_rows
-                    }
-                  }
-                `,
-        { _id: sorted_ids[i].id }
+          mutation IncreaseIds($_id: Int) {
+            update_weekly(where: { id: { _eq: $_id } }, _inc: { id: 1 }) {
+              affected_rows
+            }
+          }
+        `,
+        { _id: sorted_ids[i].id },
       );
     }
     await client.request(
       gql`
-              mutation Insert_Weekly_One($id: Int, $title: String, $url: String) {
-                insert_weekly_one(object: {id: $id, title: $title, url: $url}) {
-                  id
-                  title
-                  url
-                }
-              }
-            `,
-      { id: req.body.id + 1, title: title, url: req.body.url }
+        mutation Insert_Weekly_One($id: Int, $title: String, $url: String) {
+          insert_weekly_one(object: { id: $id, title: $title, url: $url }) {
+            id
+            title
+            url
+          }
+        }
+      `,
+      { id: req.body.id + 1, title: title, url: req.body.url },
     );
     return res.status(200).send("ok");
   } catch (err) {
     return res.status(500).send("500 Internal Server Error: " + err);
   }
-})
+});
 
-router.post("/delete",authenticate(["counselor"]), async (req, res) => {
+router.post("/delete", authenticate(["counselor"]), async (req, res) => {
   try {
-    if (!req.body.id) return res.status(400).send("400 Bad Request: not enough params!");
+    if (!req.body.id)
+      return res.status(400).send("400 Bad Request: not enough params!");
     const QueryGreaterIds: any = await client.request(
       gql`
-              query QueryGreaterIds($_id: Int) {
-                weekly(where: {id: {_gt: $_id}}) {
-                  id
-                }
-              }
-            `,
-      { _id: req.body.id }
+        query QueryGreaterIds($_id: Int) {
+          weekly(where: { id: { _gt: $_id } }) {
+            id
+          }
+        }
+      `,
+      { _id: req.body.id },
     );
     const sorted_ids = [...QueryGreaterIds.weekly];
     sorted_ids.sort((a: any, b: any) => {
       return a.id - b.id;
-    })
+    });
     await client.request(
       gql`
-              mutation Delete_Weekly_One($_id: Int) {
-                delete_weekly(where: {id: {_eq: $_id}}) {
-                  affected_rows
-                }
-              }
-            `,
-      { _id: req.body.id }
+        mutation Delete_Weekly_One($_id: Int) {
+          delete_weekly(where: { id: { _eq: $_id } }) {
+            affected_rows
+          }
+        }
+      `,
+      { _id: req.body.id },
     );
     for (let i = 0; i < sorted_ids.length; i++) {
       await client.request(
         gql`
-                  mutation IncreaseIds($_id: Int) {
-                    update_weekly(where: {id: {_eq: $_id}}, _inc: {id: -1}) {
-                      affected_rows
-                    }
-                  }
-                `,
-        { _id: sorted_ids[i].id }
+          mutation IncreaseIds($_id: Int) {
+            update_weekly(where: { id: { _eq: $_id } }, _inc: { id: -1 }) {
+              affected_rows
+            }
+          }
+        `,
+        { _id: sorted_ids[i].id },
       );
     }
     return res.status(200).send("ok");
   } catch (err) {
     return res.status(500).send("500 Internal Server Error: " + err);
   }
-})
+});
 
-router.post("/init",authenticate(["counselor"]), async (req, res) => {
+router.post("/init", authenticate(["counselor"]), async (req, res) => {
   try {
-    if (!req.body.data) return res.status(400).send("400 Bad Request: no data provided!");
+    if (!req.body.data)
+      return res.status(400).send("400 Bad Request: no data provided!");
     await client.request(
       gql`
-              mutation Init($objects: [weekly_insert_input!] = {}) {
-                insert_weekly(objects: $objects) {
-                  affected_rows
-                }
-              }
-            `,
-      { objects: req.body.data }
+        mutation Init($objects: [weekly_insert_input!] = {}) {
+          insert_weekly(objects: $objects) {
+            affected_rows
+          }
+        }
+      `,
+      { objects: req.body.data },
     );
     return res.status(200).send("ok");
   } catch (err) {
     return res.status(500).send("500 Internal Server Error: " + err);
   }
-})
+});
 
 export default router;
 
