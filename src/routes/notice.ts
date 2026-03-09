dbc8f05e79da84d8e270af556ad3b585d24b8f6a
nozomizo1314 <lenin1917117@163.com>
2026-03-09 22:05:01 +0800

    feat:change mutation from using graphql to api (#1714)

--- src/routes/notice.ts
+++ src/routes/notice.ts
@@ -3,29 +3,37 @@ import authenticate from "../middlewares/authenticate";
 import * as noticeFunc from "../hasura/notice";
 const router = express.Router();
 //TODO: Optional parameters should be implemented.
-router.post("/update", authenticate(["counselor","root"]), async (req, res) => {
-  try {
-    const title: string = req.body.title;
-    const content: string = req.body.content;
-    const files: string = req.body.files;
-    const notice_type: string = req.body.notice_type;
-    const notice_id = req.body.id;
-    const updateFields : any = {};
-    if (title) updateFields.title = title;
-    if (content) updateFields.content = content;
-    if (files) updateFields.files = files;
-    if (notice_type) updateFields.notice_type = notice_type;
-    console.log(updateFields);
-    if(!notice_id || updateFields.length === 0)
-    {
-      return res.status(422).send("422 Unprocessable Entity: Missing credentials");
+router.post(
+  "/update",
+  authenticate(["counselor", "root"]),
+  async (req, res) => {
+    try {
+      const title: string = req.body.title;
+      const content: string = req.body.content;
+      const files: string = req.body.files;
+      const notice_type: string = req.body.notice_type;
+      const notice_id = req.body.id;
+      const updateFields: any = {};
+      if (title) updateFields.title = title;
+      if (content) updateFields.content = content;
+      if (files) updateFields.files = files;
+      if (notice_type) updateFields.notice_type = notice_type;
+      console.log(updateFields);
+      if (!notice_id || updateFields.length === 0) {
+        return res
+          .status(422)
+          .send("422 Unprocessable Entity: Missing credentials");
+      }
+      const notice_uuid: string = await noticeFunc.update_notice(
+        notice_id,
+        updateFields,
+      );
+      return res.status(200).send(notice_uuid);
+    } catch (err) {
+      return res.status(500).send("Internal Server Error");
     }
-    const notice_uuid: string = await noticeFunc.update_notice(notice_id,updateFields);
-    return res.status(200).send(notice_uuid);
-  } catch (err) {
-    return res.status(500).send("Internal Server Error");
-  }
-});
+  },
+);
 
 //router.post("/update", authenticate(["counselor","root"]), async (req, res) => {
 //  try {
@@ -44,17 +52,48 @@ router.post("/update", authenticate(["counselor","root"]), async (req, res) => {
 //  }
 //});
 
-router.post("/delete", authenticate(["counselor","root"]), async (req, res) => {
+//newly added
+router.post("/add", authenticate(["counselor", "root"]), async (req, res) => {
   try {
-    const id: string = req.body.id;
-    if (!id) {
-      return res.status(422).send("422 Unprocessable Entity: Missing credentials");
+    const title: string = req.body.title;
+    const content: string = req.body.content;
+    const files: string = req.body.files;
+    const notice_type: string = req.body.notice_type;
+
+    if (!title || !content || !files || !notice_type) {
+      return res
+        .status(422)
+        .send("422 Unprocessable Entity: Missing credentials");
     }
-    const notice_id = await noticeFunc.delete_notice(id);
+    const notice_id: string = await noticeFunc.add_notice(
+      title,
+      content,
+      files,
+      notice_type,
+    );
     return res.status(200).send(notice_id);
   } catch (err) {
     return res.status(500).send("Internal Server Error");
   }
 });
+
+router.post(
+  "/delete",
+  authenticate(["counselor", "root"]),
+  async (req, res) => {
+    try {
+      const id: string = req.body.id;
+      if (!id) {
+        return res
+          .status(422)
+          .send("422 Unprocessable Entity: Missing credentials");
+      }
+      const notice_id = await noticeFunc.delete_notice(id);
+      return res.status(200).send(notice_id);
+    } catch (err) {
+      return res.status(500).send("Internal Server Error");
+    }
+  },
+);
 
 export default router;
